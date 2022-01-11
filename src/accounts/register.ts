f8ed99600490ec77c0a122db4f69a2a2334727ef
the-abe-train <abraham.train@gmail.com>
2022-01-11 09:33:15 -0500

    Added encryption

--- src/accounts/register.ts
+++ src/accounts/register.ts
@@ -1,6 +1,17 @@
+import bcrypt from 'bcryptjs';
+const {genSalt, hash} = bcrypt;
+
 export async function registerUser(
   name: string,
   password: string
 ): Promise<string> {
-  return "userId";
+
+  // Generate salt
+  const salt = await genSalt(10);
+
+  // Hash with salt
+  const hashedPassword = await hash(password, salt);
+
+
+  return hashedPassword;
 }
